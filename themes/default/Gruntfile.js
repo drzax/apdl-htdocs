ffee46fdfe0dc767ec0d3e855d660677defb3bfb
Simon Elvery <simon@elvery.net>
2013-11-17 15:29:10 +1000

    New Gruntfile Add linking and re-arrange front-end output.

--- themes/default/Gruntfile.js
+++ themes/default/Gruntfile.js
@@ -5,23 +5,24 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		uglify: {
-			library: {
+			lib: {
 				options: {
 					preserveComments: 'some',
-					mangle: false,
-					compress: false,
-					beautify: true
+					mangle: true,
+					compress: true,
+					beautify: false
 				},
 				files: {
-					'javascript/html5shiv.js': ['src/javascript/lib/html5shiv/src/html5shiv-printshiv.js'],
-					'javascript/require.js': ['src/javascript/require-config.js', 'src/javascript/lib/require/require.js'],
-					'javascript/jquery.js': ['src/javascript/lib/jquery/jquery-2.0.3.js'],
-					'javascript/jquery.autocomplete.js': ['src/javascript/lib/jquery-autocomplete/dist/jquery.autocomplete.js'],
-					'javascript/d3.js': ['src/javascript/lib/d3/d3.v3/d3.v3.js'],
-					'javascript/svg-icons.js': ['src/javascript/lib/snap/dist/snap.svg.js','src/javascript/lib/svgicons.js']
+					'javascript/lib.js': [
+						'src/javascript/lib/jquery/jquery-2.0.3.js',
+						'src/javascript/lib/jquery-autocomplete/dist/jquery.autocomplete.js',
+						'src/javascript/lib/d3/d3.v3/d3.v3.js',
+						'src/javascript/lib/snap/dist/snap.svg.js',
+						'src/javascript/lib/svgicons.js'
+					]
 				}
 			},
-			project: {
+			app: {
 				options: {
 					preserveComments: 'some',
 					mangle: false,
@@ -29,10 +30,15 @@ module.exports = function(grunt) {
 					beautify: true
 				},
 				files: {
-					'javascript/main.js': ['src/javascript/main.js'],
-					'javascript/svgicons-config.js': ['src/javascript/svgicons-config.js']
+					'javascript/app.js': [
+						'../../persona/javascript/frontend.js',
+						'src/javascript/svgicons-config.js',
+						'src/javascript/menu.js',
+						'src/javascript/search.js',
+						'src/javascript/main.js'
+					]
 				}
-			}
+			},
 		},
 		compass: {
 			production: {
@@ -51,14 +57,17 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		jshint: {
+			app: ['Gruntfile.js', 'src/javascript/*.js']
+		},
 		watch: {
 			scriptsLibrary: {
 				files: ['src/javascript/lib/**/*.js'],
-				tasks: 'uglify:library'
+				tasks: ['jshint:lib', 'uglify:lib']
 			},
 			scriptsProject: {
-				files: ['src/javascript/*.js', 'src/javascript/**/*.js', '!src/javascript/lib/**'],
-				tasks: 'uglify:project'
+			files: ['src/javascript/*.js'],
+				tasks: ['jshint:app', 'uglify:app']
 			},
 			styles: {
 				files: ['src/scss/*.scss', 'src/scss/**/*.scss'],
@@ -71,5 +80,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 
 };
